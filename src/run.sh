369df6c162cf962988adea9f053b805839e29dde
krishnan-tech <krishnannavadia@gmail.com>
2021-01-11 13:06:53 +0530

    ➕ Added all models in ./src/run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,8 @@
-python train.py --fold 4 --model logistic_regression
+python train.py --fold 4 --model decision_tree_gini
+python train.py --fold 4 --model decision_tree_entropy
+python train.py --fold 4 --model rf
+python train.py --fold 4 --model svm
+python train.py --fold 4 --model naive_bayes
+python train.py --fold 4 --model xgboost
+python train.py --fold 4 --model knn
 read -p "Thank you 🙏!! Press any key to exit.😄"
